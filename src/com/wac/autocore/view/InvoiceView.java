5d5c0e2ba29760b9e61ac93d68698a31dac79c0b
Simon Toivola <simontoivola@outlook.com>
2026-09-16 18:54:09 +0200

    created CreateInvoiceDialogue and using it in InvoiceView

--- src/com/wac/autocore/view/InvoiceView.java
+++ src/com/wac/autocore/view/InvoiceView.java
@@ -1,18 +1,25 @@
 package com.wac.autocore.view;
 
+import com.wac.autocore.data.Database;
 import com.wac.autocore.model.Invoice;
 import com.wac.autocore.service.GarageSystem;
+import com.wac.autocore.view.dialog.CreateInvoiceDialog;
+import com.wac.autocore.view.util.AlertHelper;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
-import java.util.Observable;
+import java.time.LocalDate;
 
 /**
  * <b>InvoiceView</b>
@@ -20,14 +27,17 @@ import java.util.Observable;
  */
 public class InvoiceView extends VBox {
 
-    GarageSystem garageSystem = new GarageSystem();
+    private final GarageSystem garageSystem = new GarageSystem();
 
-    ObservableList<Invoice> invoiceMasterData = FXCollections.observableArrayList();
+    private final ObservableList<Invoice> invoiceMasterData = FXCollections.observableArrayList();
+    private final SortedList<Invoice> sortedData = new SortedList<>(invoiceMasterData);
 
-    TableView<Invoice> invoiceTable = new TableView<>();
+    private final TableView<Invoice> invoiceTable = new TableView<>();
+
+    private final Button btnCreateInvoice = new Button("Create");
+
 
     public InvoiceView() {
-
         this.getStyleClass().add("content-area");
         this.setSpacing(20);
         this.setPadding(new Insets(20));
@@ -37,7 +47,108 @@ public class InvoiceView extends VBox {
         Label title = new Label("Invoices");
         title.getStyleClass().add("text-title");
 
-        this.getChildren().addAll(title, invoiceTable);
+        this.loadMasterData();
+        this.initializeTable();
+
+        HBox buttonBar = this.createButtonBar();
+
+        this.getChildren().addAll(title, buttonBar, invoiceTable);
     }
 
+    private void loadMasterData() {
+        this.invoiceMasterData.setAll(Database.getInvoices());
+    }
+
+    private void refreshData() {
+        this.invoiceMasterData.setAll(Database.getInvoices());
+    }
+
+    private void initializeTable() {
+
+        TableColumn<Invoice, Integer> idCol = new TableColumn<>("ID");
+        idCol.setCellValueFactory(c ->
+                new SimpleIntegerProperty(c.getValue().getId()).asObject()
+        );
+
+        TableColumn<Invoice, Integer> workOrderIdCol = new TableColumn<>("Order-Id");
+        workOrderIdCol.setCellValueFactory(c ->
+                new SimpleIntegerProperty(c.getValue().getWorkOrderId()).asObject()
+        );
+
+        TableColumn<Invoice, LocalDate> dateCol = new TableColumn<>("Date");
+        dateCol.setCellValueFactory(c ->
+                new SimpleObjectProperty<>(c.getValue().getInvoiceDate())
+        );
+
+        TableColumn<Invoice, Double> amountCol = new TableColumn<>("Amount");
+        amountCol.setCellValueFactory(c ->
+                new SimpleDoubleProperty(c.getValue().getAmount()).asObject()
+        );
+
+        TableColumn<Invoice, Double> discountCol = new TableColumn<>("Discount");
+        discountCol.setCellValueFactory(c ->
+                new SimpleDoubleProperty(c.getValue().getDiscount()).asObject()
+        );
+
+        TableColumn<Invoice, Double> totalCol = new TableColumn<>("Total");
+        totalCol.setCellValueFactory(c ->
+                new SimpleDoubleProperty(c.getValue().getTotalAmount()).asObject()
+        );
+
+        TableColumn<Invoice, Boolean> paidCol = new TableColumn<>("Paid");
+        paidCol.setCellValueFactory(c ->
+                new SimpleBooleanProperty(c.getValue().isPaid()).asObject()
+        );
+        paidCol.setCellFactory(col -> new TableCell<Invoice, Boolean>() {
+            @Override
+            protected void updateItem(Boolean paid, boolean empty) {
+                super.updateItem(paid, empty);
+                if (empty || paid == null) {
+                    setText(null);
+                } else {
+                    setText(paid ? "Yes" : "No");
+                }
+            }
+        });
+
+        this.invoiceTable.getColumns().addAll(
+                idCol, workOrderIdCol, dateCol, amountCol, discountCol, totalCol, paidCol
+        );
+
+        this.invoiceTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        this.sortedData.comparatorProperty().bind(this.invoiceTable.comparatorProperty());
+        this.invoiceTable.setItems(this.sortedData);
+    }
+
+    private HBox createButtonBar() {
+        String btnPrimary = "btn-primary";
+
+        btnCreateInvoice.getStyleClass().addAll("btn", btnPrimary);
+        btnCreateInvoice.setOnAction(event -> this.openCreateInvoiceDialog());
+
+
+
+
+        HBox box = new HBox(15, btnCreateInvoice);
+        box.setPadding(new Insets(15, 0, 0, 0));
+        box.setAlignment(Pos.CENTER_LEFT);
+        return box;
+    }
+
+    private void openCreateInvoiceDialog() {
+        CreateInvoiceDialog dialog = new CreateInvoiceDialog();
+        dialog.showAndWait().ifPresent(result -> {
+            Invoice inv = garageSystem.createInvoice(
+                    result.getWorkOrderId(),
+                    result.getDiscountCode()
+            );
+            if (inv != null) {
+                refreshData();
+                AlertHelper.showInfo("Invoice created", "A new invoice has been created");
+            } else {
+                AlertHelper.showError("Could not create", "Check that work order is COMPLETED");
+            }
+        });
+    }
 }
--- src/com/wac/autocore/view/dialog/CreateInvoiceDialog.java
+++ src/com/wac/autocore/view/dialog/CreateInvoiceDialog.java
@@ -1,8 +1,107 @@
 package com.wac.autocore.view.dialog;
 
+import com.wac.autocore.data.Database;
+import com.wac.autocore.model.WorkOrder;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.util.StringConverter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * <b>CreateInvoiceDialog</b>
  * <p>Ansvar: Dialog för att skapa en faktura.</p>
  */
-public class CreateInvoiceDialog {
+public class CreateInvoiceDialog extends Dialog<CreateInvoiceDialog.Result> {
+
+    private final ComboBox<WorkOrder> workOrderCombo = new ComboBox<>();
+    private final TextField discountField = new TextField();
+
+    private final ButtonType saveButtonType = new ButtonType("Save", ButtonBar.ButtonData.OK_DONE);
+
+    public CreateInvoiceDialog() {
+        setTitle("New Invoice");
+        setHeaderText("Create Invoice");
+
+        getDialogPane().getStylesheets().add(
+                getClass().getResource("/com/wac/autocore/view/style.css").toExternalForm()
+        );
+
+        getDialogPane().getButtonTypes().addAll(saveButtonType, ButtonType.CANCEL);
+
+        // -------------------------------------------------------------------------------------------------------------
+
+        List<WorkOrder> completed = Database.getWorkOrders().stream()
+                .filter(wo -> "COMPLETED".equals(wo.getStatus()))
+                .collect(Collectors.toList());
+        workOrderCombo.getItems().addAll(completed);
+
+        workOrderCombo.setConverter(new StringConverter<WorkOrder>() {
+            @Override
+            public String toString(WorkOrder wo) {
+                return wo == null ? "" : "#" + wo.getId() + " (" + wo.getStatus() + ")";
+            }
+
+            @Override
+            public WorkOrder fromString(String string) {
+                return null;
+            }
+        });
+
+        // -------------------------------------------------------------------------------------------------------------
+
+        discountField.setPromptText("Discount code (optional)");
+
+        // -------------------------------------------------------------------------------------------------------------
+
+        Node saveButton = getDialogPane().lookupButton(saveButtonType);
+        saveButton.setDisable(true);
+
+        workOrderCombo.valueProperty().addListener((obs, oldV, newV) -> validate(saveButton));
+
+        VBox content = new VBox(12);
+        content.setPadding(new Insets(16));
+        content.getChildren().addAll(
+                new Label("Work order"), workOrderCombo,
+                new Label("Discount code"), discountField
+        );
+        getDialogPane().setContent(content);
+
+        setResultConverter(buttonType -> {
+            if (buttonType == saveButtonType) {
+                int workOrderId = workOrderCombo.getValue().getId();
+                String code = discountField.getText();
+                return new Result(workOrderId, code);
+            }
+            return null;
+        });
+    }
+
+    private void validate(Node saveButton) {
+        boolean valid = workOrderCombo.getValue() != null;
+        saveButton.setDisable(!valid);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public static class Result {
+        private final int workOrderId;
+        private final String discountCode;
+
+        public Result(int workOrderId, String discountCode) {
+            this.workOrderId = workOrderId;
+            this.discountCode = discountCode;
+        }
+
+        public int getWorkOrderId() {
+            return workOrderId;
+        }
+
+        public String getDiscountCode() {
+            return discountCode;
+        }
+    }
 }
