c017add20cd6bdbfcced873c3aa0f872ff672c9e
Alexander Nilsson <error_alex@hotmail.com>
2026-09-24 13:56:24 +0200

    WorkOrderService and Repository

---
+++ src/main/java/com/wac/autocore/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.wac.autocore.repository;
+
+import com.wac.autocore.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
+}
