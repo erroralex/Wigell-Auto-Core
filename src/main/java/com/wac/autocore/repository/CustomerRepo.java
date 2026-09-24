94917773ba55b80415e82bc72e14d613b2a6c226
Amie Molin <114564779+fizzigood@users.noreply.github.com>
2026-09-25 01:45:41 +0200

    Create CustomerRepo.java

---
+++ src/main/java/com/wac/autocore/repository/CustomerRepo.java
@@ -0,0 +1,8 @@
+package com.wac.autocore.repository;
+
+import com.wac.autocore.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer, Integer> {}
